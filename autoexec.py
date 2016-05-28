44f36a5698f155434376de1eed768de42c24b771
Chris Cartwright <cartwright.christopher@gmail.com>
2016-05-28 13:19:21 -0400

    Fixed random shuffle

--- autoexec.py
+++ autoexec.py
@@ -1,6 +1,7 @@
 
 import xbmc
 import xml.dom.minidom
+import random
 
 # Can't find a way to play the favourites list - manual approach it is
 tree = xml.dom.minidom.parse("/storage/.kodi/userdata/favourites.xml")
@@ -10,14 +11,19 @@ datas = [x.childNodes[0].data for x in nodes]
 # Get just the file name
 files = [x[11:-2] for x in datas]
 
+# SystemRandom does appear to be random at each boot
+rand = random.SystemRandom()
+rand.shuffle(files)
+
 songList = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
 songList.clear()
 
 for f in files:
     songList.add(f)
 
-# This shuffles in the same order every time. Bugger.
-songList.shuffle()
+# This shuffle appears to use time()
+# time will always be the same value because the RPi doesn't have an RTC
+#songList.shuffle()
 
 xbmc.executebuiltin("xbmc.playercontrol(RepeatAll)")
 
